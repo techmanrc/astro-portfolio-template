25e868ee777d7d11df022345ec793ad1d08eeac8
Richard Cyrus <techmanrc@gmail.com>
2023-05-31 23:48:01 -0400

    chore: configure stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-html'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'declaration-block-no-redundant-longhand-properties': [
+      true,
+      { ignoreShorthands: ['/flex/', '/grid/'] }
+    ],
+    'font-family-name-quotes': 'always-where-required',
+    'comment-empty-line-before': [
+      'always',
+      {
+        ignoreComments: ['/^#endregion/'],
+        except: ['first-nested'],
+        ignore: ['after-comment', 'stylelint-commands']
+      }
+    ],
+    'number-max-precision': 6,
+    'order/properties-alphabetical-order': true,
+    'selector-class-pattern': null
+  }
+}
